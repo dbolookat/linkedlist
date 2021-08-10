94211e7bba740cfa8cc56a4332b99bb94e0c2c26
danial <d.bolookat@gmail.com>
2021-08-10 13:50:40 +0430

    create findItem method

--- src/SampleLinkedList.java
+++ src/SampleLinkedList.java
@@ -1,3 +1,17 @@
-public class SampleLinkedList {
+public class SampleLinkedList<E> {
+    private Node<E> head;
+
+    public int findItem(E item){
+        Node findItem = head;
+        int index=0;
+        while (findItem!=null && findItem.getItem() != item){
+            findItem =findItem.getNext();
+            index++;
+        }
+        if (findItem==null){
+            return -1;
+        }
+        return index;
+    }
 
 }
