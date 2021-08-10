a06d206a224db30a6d97f66be0d5de911cf53b63
danial <d.bolookat@gmail.com>
2021-08-10 13:45:17 +0430

    create Node class

---
+++ src/Node.java
@@ -0,0 +1,25 @@
+public class Node<E> {
+    E item;
+    Node<E> next;
+
+    public Node(E item, Node<E> next) {
+        this.item = item;
+        this.next = next;
+    }
+
+    public E getItem() {
+        return item;
+    }
+
+    public Node<E> getNext() {
+        return next;
+    }
+
+    public void setItem(E item) {
+        this.item = item;
+    }
+
+    public void setNext(Node<E> next) {
+        this.next = next;
+    }
+}
